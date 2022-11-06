f71bbb0b2665ed2e1ffa239e67064b815725a029
vyshnavi-18 <106963448+vyshnavi-18@users.noreply.github.com>
2022-11-06 23:34:19 +0530

    Add files via upload

---
+++ Alarm.py
@@ -0,0 +1,14 @@
+# import datetime
+# from playsound import playsound
+# alarmH = int(input("Which time do you want the alarm to ring? "))
+# alarmM = int(input("On which minute do you want the alarm to ring? "))
+# amPm = str(input("am or pm? "))
+# print("Waiting for alarm",alarmH,alarmM,amPm)
+# if (amPm == "pm"):
+#         alarmH = alarmH + 12
+# while(12 == 12):
+#     if(alarmH == datetime.datetime.now().hour and
+#         alarmM == datetime.datetime.now().minute) :
+#         print("Time to wake up")
+#         playsound("/home/anjalikumari/Downloads/Pavitra")
+#         break
